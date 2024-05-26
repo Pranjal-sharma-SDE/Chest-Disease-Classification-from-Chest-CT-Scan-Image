83a8f082d7c93a395eac653c8cc4d1c7af7e40b3
Pranjal Sharma <pranjal.software.engineer@gmail.com>
2024-05-26 11:07:05 +0530

    update

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Chest-Disease-Classification-from-Chest-CT-Scan-Image"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "Pranjal"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "pranjal1476772@gmail.com"
 
 
 setuptools.setup(
